970fcbc5504776659896dc06b3f108e268b15657
Michael <sie20003@byui.edu>
2023-01-05 14:24:52 +1100

    Completed Lesson 1

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,7 @@
-displayStartMessage = (req, res) => {
+function displayStartMessage (req, res) {
    const data = 'Hello, A. Michael Sierra!!!';
    res.status(200).send(data);
-};
+}
 
 module.exports = {
    displayStartMessage,
